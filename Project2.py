73c93a6f06d3a80779cb7b9d659674e38579a5f2
Jessika Solleder <jessikasolleder@u.boisestate.edu>
2024-03-02 23:38:20 -0700

    progress

--- Project2.py
+++ Project2.py
@@ -25,7 +25,9 @@ def main():
     Phi = simpledialog.askfloat("Input", "Enter the value of \u03c6 ' in degrees.")
 
 # # Calculating Dimensions of Rigid Retaining Wall Based on User Input of Height # #
+
     global StemHeight, StemBottom, SlabThickness, Heel, Toe, SoilHeight
+    
     StemHeight = 1 * Height
     StemBottom = 0.5 * Height
     SlabThickness = 0.1 * Height
@@ -33,24 +35,21 @@ def main():
     Toe = 0.3 * Height
     SoilHeight = SlabThickness + StemHeight + Heel + np.tan(Alpha)
  
-def rankine_analysis(Height, Gamma, Alpha, Phi):
-    # Recall the dimensions of the rigid retaining wall
-
-    StemHeight = 1 * Height
-    StemBottom = 0.5 * Height
-    SlabThickness = 0.1 * Height
-    Heel = 0.1 * Height
-    Toe = 0.3 * Height
-    SoilHeight = SlabThickness + StemHeight + Heel + np.tan(Alpha)
+def rankine_analysis(SoilHeight, Gamma, Alpha, Phi):
+    
+    global RPv, RPh, RPa, RPa, RKp, RKa
     
     # Convert angles from degrees to radians
+    
+    global Phi_rad, Alpha_rad
+    
     Phi_rad= np.radians(Phi)
     Alpha_rad= np.radians(Alpha)
 
     # Calculate the coefficient (Ka) of the active earth pressure using Rakine
     RKa = (1 - np.sin(Phi_rad)) / (1 + np.sin(Phi_rad))
 
-    # Calculate Rankine passive earth pressure coefficient (Kp)
+    # Calculate the coefficient (Kp) of the passive earth pressure using Rakine
     RKp = (1 + np.sin(Phi_rad)) / (1 - np.sin(Phi_rad))
 
     # Calculate Rankine Lateral Earth Pressures for the active and passive conditions
@@ -64,19 +63,18 @@ def rankine_analysis(Height, Gamma, Alpha, Phi):
 def coloumb_pressure():
 
  # Calculate the coefficient (Ka) of the active earth pressure using Coulomb
- 
     CKa = ((np.cos(Phi)) ** 2) / (np.cos(Alpha)*(1+((np.sin(Phi + Alpha) * (np.sin(Phi - Alpha)))/(np.cos(Alpha)*(np.cos(-Alpha))) ** 0.5) ** 2))
 
-# Calculate Coulomb passive earth pressure coefficient (Kp)
-
+ # Calculate the coefficient (Kp) of the passive earth pressure using Coulomb
+    CKp = (1 + np.sin(Phi_rad)) / (1 - np.sin(Phi_rad))
 # Calculate Coulomb Lateral Earth Pressures for the active and passive conditions
-
+    CPp = 0.5 * RKp * (Gamma * (SoilHeight ** 2))
     CPa = 0.5 * (CKa*Gamma*(SoilHeight ** 2))
-    CPv = CPa * np.sin(Alpha)
     
 # Calculate Coulomb horizontal & vertical components of the active earth pressure from above 
 
-
+    CPv = CPa * np.sin(Alpha)
+    CPh = CPa * np.cos(Alpha)
 
 
 if __name__ == "__main__":
