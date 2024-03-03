9a27012128c5d165d6e0d9555bdb35767f5b5e0f
Jessika Solleder <jessikasolleder@u.boisestate.edu>
2024-03-02 22:44:07 -0700

    progress

--- Project2.py
+++ Project2.py
@@ -14,41 +14,20 @@ def main():
     root.withdraw()  # Hide the root window
 
     # Prompt the user to input dimensions using a pop up window
+    global Height, Gamma, Alpha, Phi
     Height = simpledialog.askfloat("Input", "Enter the height of the stem (H) in feet.")
+    Gamma = simpledialog.askfloat("Input", "Enter the value of \u03B3 in lbs/cubic foot.")
+    Alpha = simpledialog.askfloat("Input", "Enter the value of \u03B1 in degrees.")
+    Phi = simpledialog.askfloat("Input", "Enter the value of \u03c6 ' in degrees.")
+
 
     Stemheight = 1 * Height
     Stembottom = 0.5 * Height
     Slabthickness = 0.1 * Height
     Heel = 0.1 * Height
     Toe = 0.3 * Height
-    Soilheight = Slabthickness + Stemheight + Heel + np.tan * (Alpha)
-
-# Display the result in a message box
-    tk.messagebox.showinfo("Confirmed", f"You input: {Height}!")
-
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
-
-    # Prompt the user to input dimensions using a popup window
-    Gamma = simpledialog.askfloat("Input", "Enter the value of \u03B3 in lbs/cubic foot.")
-    Alpha = simpledialog.askfloat("Input", "Enter the value of \u03B1 in degrees." )
-    # Display the result in a message box
-    tk.messagebox.showinfo("Confirmed", f"You input: {Gamma } for \u03B3 !")
-
-
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
-
-    # Prompt the user to input dimensions using a popup window
-    Phi = simpledialog.askfloat("Input", "Enter the value of \u03c6 ' in degrees.")
-
-    # Display the result in a message box
-    tk.messagebox.showinfo("Confirmed", f"You input: {Phi} for \u03c6 !")
-    
-    
-
-if __name__ == "__main__":
-    main()
+    Height = Slabthickness + Stemheight + Heel + np.tan * (Alpha)
+ 
     
 # # Calculating Dimensions of Rigid Retaining Wall Based on User Input of Height # #
 
@@ -61,15 +40,23 @@ def rankine_coefficients():
     Phi = np.radians(Phi)
     Alpha = np.radians(Alpha)
 
-    # Calculate active earth pressure using Rakine
+    Stemheight = 1 * Height
+    Stembottom = 0.5 * Height
+    Slabthickness = 0.1 * Height
+    Heel = 0.1 * Height
+    Toe = 0.3 * Height
+    Height = Slabthickness + Stemheight + Heel + np.tan * (Alpha)
+    
+
+    # Calculate the coefficient (Ka) of the active earth pressure using Rakine
     Rka = (1 - np.sin(Phi)) / (1 + np.sin(Phi))
 
-    # Calculate Rankine active and passive coefficients
+    # Calculate Rankine passive earth pressure coefficient (Kp)
     Rkp = (1 + np.sin(Phi)) / (1 - np.sin(Phi))
 
-    # Calculate Rankine Lateral Earth Pressure
-    Rpa = 0.5 * Ka * ( Gamma * (Height ** 2))
-    Rpp = 0.5 * Kp * ( Gamma * (Height ** 2))
+    # Calculate Rankine Lateral Earth Pressures for the active and passive conditions
+    Rpa = 0.5 * Rka * ( Gamma * (Height ** 2))
+    Rpp = 0.5 * Rkp * ( Gamma * (Height ** 2))
 
     # Calculate Rankine horizontal & vertical components of the active earth pressure from above
     Rpv = Rpa * np.sin(Alpha)
@@ -78,17 +65,16 @@ def rankine_coefficients():
     
 def coloumb_pressure():
 
- # Calculate active earth pressure using Coulomb
-    Rka = (1 - np.sin(Phi)) / (1 + np.sin(Phi))
+ # Calculate the coefficient (Ka) of the active earth pressure using Coulomb
+ 
+    Cka = ((np.cos(Phi)) ** 2) / (np.cos(Alpha)*(1+((np.sin(Phi + Alpha) * (np.sin(Phi - Alpha)))/(np.cos(Alpha)*(np.cos(-Alpha))) ** 0.5) ** 2))
 
-    # Calculate Rankine active and passive coefficients
-    Rkp = (1 + np.sin(Phi)) / (1 - np.sin(Phi))
+# Calculate Coulomb passive earth pressure coefficient (Kp)
 
-    # Calculate Rankine Lateral Earth Pressure
-    Rpa = 0.5 * Ka * ( Gamma * (Height ** 2))
-    Rpp = 0.5 * Kp * ( Gamma * (Height ** 2))
+# Calculate Coulomb Lateral Earth Pressures for the active and passive conditions
 
-    # Calculate Rankine Horizontal & Vertical Components of the Active Earth Pressure from Above
-    Rpv = Rpa * np.sin(Alpha)
-    Rph = Rpa * np.cos(Alpha)
+# Calculate Coulomb horizontal & vertical components of the active earth pressure from above 
 
+
+if __name__ == "__main__":
+    main()
